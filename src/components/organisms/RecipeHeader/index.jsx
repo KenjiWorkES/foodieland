e97cb1dca1519eb506bcc57bcf4a19e5a6f9fd14
KenjiWorkES <lucas.kenjies@gmail.com>
2022-07-22 10:22:27 -0300

    colocando component circularButton no RecipeHeader

--- src/components/organisms/RecipeHeader/index.jsx
+++ src/components/organisms/RecipeHeader/index.jsx
@@ -1,4 +1,5 @@
 import { RecipeList } from "../../molecules";
+import { CircularButton } from "../../atoms";
 import "./recipeHeader.scss";
 
 const RecipeHeader = () => {
@@ -6,6 +7,10 @@ const RecipeHeader = () => {
     <section className="recipeHeader">
       <h1 className="recipeHeader__title">Health Japanese Fried Rice</h1>
       <RecipeList />
+      <div className="recipeHeader__actions">
+        <CircularButton label="print" image="/assets/printer.svg" />
+        <CircularButton label="share" image="/assets/share.svg" />
+      </div>
     </section>
   );
 };
